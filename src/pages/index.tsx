d26fe31d7786f4cacbb909ca41bdcf723da1ac78
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-05 23:16:23 -0400

    feat: Add getServerSideProps for fetch

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from 'next';
+interface HomeProps {
+  count: number;
+}
+export default function Home({ count }: HomeProps) {
+  return (
+    <h1 className="text-violet-500 font-bold text-4xl">
+      Contagem: {count}
+    </h1>
+  );
+}
+export const getServerSideProps: GetServerSideProps =
+  async () => {
+    const response = await fetch(
+      'http://localhost:3333/pools/count'
+    );
+    const data = await response.json();
+    return {
+      props: {
+        count: data.count
+      }
+    };
+  };
